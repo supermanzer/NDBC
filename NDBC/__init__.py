aedbd376479946b24f320b445df365182feea5ed
Ryan <ryan.manzer@gmail.com>
2020-06-28 09:59:21 -0700

    Bumped to version 1.0.0 with Python 3.7 requirement (breaking change).

--- NDBC/__init__.py
+++ NDBC/__init__.py
@@ -1,2 +1,2 @@
 # Version of the realpython-reader package
-__version__ = "0.1.1"
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NDBC",
-    version="0.1.1",
+    version="1.0.0",
     description="A package to automate the loading of NDBC data to a custom object.",
     long_description=long_description,
     long_description_content_type="text/markdown",
