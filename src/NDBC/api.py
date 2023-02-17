8acb9b5ac9f8b4535fcf6dd43257f8ca89bee563
Ryan Manzer <ryan.manzer@gmail.com>
2023-02-17 10:34:42 -0800

    creating new files for code refactoring
    This commit creates the api.py and repository.py files and adds simple file level docstrings to explain their purpose in NDBC.  Future work will add the desired functionality to these files.  The intent is to separate the concerns currently with the DataBuoy class in NDBC.py

---
+++ src/NDBC/api.py
@@ -0,0 +1,4 @@
+"""NDBC/api.py - define data retrieval and parsing functionality
+
+This file defines the functionality for making HTTP requests to retrieve NDBC data and parsing the response.
+"""
---
+++ src/NDBC/repository.py
@@ -0,0 +1,5 @@
+"""NDBC/repository.py - defines repository pattern for NDBC data persistence
+
+This file defines class(es) and methodsused to save and load the state of NDBC class instances.
+"""
+
